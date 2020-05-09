87fc176cdef28a4b99b4561b9643bb7b5edd1ef2
Thalia Nero <chrisvn00@gmail.com>
2020-05-08 21:34:00 -0400

    Update LibGUI and re-enable Loot Data Block GUI.

--- src/main/java/robosky/structurehelpers/block/LootDataBlock.java
+++ src/main/java/robosky/structurehelpers/block/LootDataBlock.java
@@ -1,16 +1,23 @@
 package robosky.structurehelpers.block;
 
+import io.netty.buffer.Unpooled;
+import robosky.structurehelpers.network.ClientStructHelpPackets;
+import robosky.structurehelpers.network.LootDataPacketData;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+
+import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
 
 public class LootDataBlock extends Block implements BlockEntityProvider {
 
@@ -36,17 +43,17 @@ public class LootDataBlock extends Block implements BlockEntityProvider {
             return ActionResult.PASS;
         }
         // until LibGUI updates
-//        if (!world.isClient()) {
-//            BlockEntity be = world.getBlockEntity(pos);
-//            if (be instanceof LootDataBlockEntity) {
-//                LootDataBlockEntity ld = (LootDataBlockEntity)be;
-//                LootDataPacketData data =
-//                    new LootDataPacketData(pos, ld.getLootTable().toString(), ld.getReplacementState());
-//                PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
-//                data.write(buf);
-//                ServerSidePacketRegistry.INSTANCE.sendToPlayer(player, ClientStructHelpPackets.LOOT_DATA_OPEN, buf);
-//            }
-//        }
+        if(!world.isClient()) {
+            BlockEntity be = world.getBlockEntity(pos);
+            if(be instanceof LootDataBlockEntity) {
+                LootDataBlockEntity ld = (LootDataBlockEntity)be;
+                LootDataPacketData data =
+                    new LootDataPacketData(pos, ld.getLootTable().toString(), ld.getReplacementState());
+                PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
+                data.write(buf);
+                ServerSidePacketRegistry.INSTANCE.sendToPlayer(player, ClientStructHelpPackets.LOOT_DATA_OPEN, buf);
+            }
+        }
         return ActionResult.SUCCESS;
     }
 }
