eb15c6385d329854b3d92e6d859d7049ec683318
Thalia Nero <chrisvn00@gmail.com>
2020-05-11 01:06:47 -0400

    Redo LootDataScreen with vanilla GUI components and remove LibGUI dependency.

--- src/main/java/robosky/structurehelpers/client/LootDataScreen.java
+++ src/main/java/robosky/structurehelpers/client/LootDataScreen.java
@@ -1,17 +1,17 @@
 package robosky.structurehelpers.client;
 
-import io.github.cottonmc.cotton.gui.client.CottonClientScreen;
-import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
-import io.github.cottonmc.cotton.gui.widget.WButton;
-import io.github.cottonmc.cotton.gui.widget.WGridPanel;
-import io.github.cottonmc.cotton.gui.widget.WLabel;
-import io.github.cottonmc.cotton.gui.widget.WTextField;
 import io.netty.buffer.Unpooled;
 import robosky.structurehelpers.block.LootDataBlockEntity;
 import robosky.structurehelpers.network.LootDataPacketData;
 import robosky.structurehelpers.network.ServerStructHelpPackets;
 
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ScreenTexts;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.resource.language.I18n;
+import net.minecraft.client.util.NarratorManager;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
@@ -21,10 +21,23 @@ import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
 /**
  * Screen class for the loot data block entity.
  */
-public class LootDataScreen extends CottonClientScreen {
+public class LootDataScreen extends Screen {
+
+    private static final int guiRadius = 150;
+
+    // relevant input fields
+    private final LootDataBlockEntity backingBe;
+    private TextFieldWidget lootTableIn;
+    private TextFieldWidget replaceIn;
+    private ButtonWidget doneBtn;
+
+    // center screen position, for convenience
+    private int centerH;
+    private int centerV;
 
     public LootDataScreen(LootDataBlockEntity be) {
-        super(new Description(be));
+        super(NarratorManager.EMPTY);
+        this.backingBe = be;
     }
 
     @Override
@@ -32,64 +45,90 @@ public class LootDataScreen extends CottonClientScreen {
         return false;
     }
 
-    /**
-     * GUI for the loot data block entity.
-     */
-    private static final class Description extends LightweightGuiDescription {
+    @Override
+    protected void init() {
+        centerH = this.width / 2;
+        centerV = this.height / 2;
+        assert this.client != null : "this.client null in LootDataScreen";
+        this.client.keyboard.enableRepeatEvents(true);
+        // loot table ID
+        lootTableIn = new TextFieldWidget(this.textRenderer,
+            centerH - guiRadius,
+            centerV - 40,
+            guiRadius * 2,
+            20,
+            new TranslatableText("gui.structure-helpers.loot_data.table"));
+        lootTableIn.setMaxLength(4096);
+        lootTableIn.setText(backingBe.getLootTable().toString());
+        lootTableIn.setChangedListener(text -> updateDoneButton());
+        this.children.add(lootTableIn);
+        // replacement state
+        replaceIn = new TextFieldWidget(this.textRenderer,
+            centerH - guiRadius,
+            centerV,
+            guiRadius * 2,
+            20,
+            new TranslatableText("gui.structure-helpers.loot_data.replace"));
+        replaceIn.setMaxLength(4096);
+        replaceIn.setText(backingBe.getReplacementState());
+        replaceIn.setChangedListener(text -> updateDoneButton());
+        this.children.add(replaceIn);
+        // done + cancel buttons
+        final int padding = 5;
+        final int btnWidth = guiRadius - padding + 1;
+        doneBtn = this.addButton(new ButtonWidget(centerH - padding - btnWidth,
+            centerV + 40,
+            btnWidth,
+            20,
+            ScreenTexts.DONE,
+            btn -> this.sendDataToServer()));
+        this.addButton(new ButtonWidget(centerH + padding,
+            centerV + 40,
+            btnWidth,
+            20,
+            ScreenTexts.CANCEL,
+            btn -> this.onClose()));
+    }
 
-        private final LootDataBlockEntity backingBe;
+    private void updateDoneButton() {
+        doneBtn.active = Identifier.isValid(lootTableIn.getText());
+    }
 
-        private final WTextField lootTableIn;
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        this.renderBackground(matrices);
+        // title
+        this.drawCenteredString(matrices,
+            this.textRenderer,
+            I18n.translate("gui.structure-helpers.loot_data.title"),
+            centerH,
+            centerV - 80,
+            0xffffff);
+        // loot table ID
+        this.drawStringWithShadow(matrices,
+            this.textRenderer,
+            I18n.translate("gui.structure-helpers.loot_data.table"),
+            centerH - guiRadius,
+            lootTableIn.y - 10,
+            0xa0a0a0);
+        lootTableIn.render(matrices, mouseX, mouseY, delta);
+        // replacement state
+        this.drawStringWithShadow(matrices,
+            this.textRenderer,
+            I18n.translate("gui.structure-helpers.loot_data.replace"),
+            centerH - guiRadius,
+            replaceIn.y - 10,
+            0xa0a0a0);
+        replaceIn.render(matrices, mouseX, mouseY, delta);
+        super.render(matrices, mouseX, mouseY, delta);
+    }
 
-        private final WTextField replaceIn;
-
-        private final WButton done;
-
-        Description(LootDataBlockEntity be) {
-            backingBe = be;
-            WGridPanel root = new WGridPanel(4);
-            this.setRootPanel(root);
-
-            WLabel title = new WLabel(new TranslatableText("gui.structure-helpers.loot_data.title"),
-                WLabel.DEFAULT_TEXT_COLOR);
-            root.add(title, 0, 0, 60, 1);
-
-            WLabel lootTableLbl = new WLabel(new TranslatableText("gui.structure-helpers.loot_data.table"),
-                WLabel.DEFAULT_TEXT_COLOR);
-            root.add(lootTableLbl, 0, 7, 60, 1);
-            lootTableIn = new WTextField();
-            lootTableIn.createPeers(this); // required to make text field focusable
-            lootTableIn.setMaxLength(4096);
-            lootTableIn.setText(be.getLootTable().toString());
-            root.add(lootTableIn, 0, 10, 60, 1);
-
-            WLabel replaceLbl = new WLabel(new TranslatableText("gui.structure-helpers.loot_data.replace"),
-                WLabel.DEFAULT_TEXT_COLOR);
-            root.add(replaceLbl, 0, 17, 60, 1);
-            replaceIn = new WTextField();
-            replaceIn.createPeers(this); // required to make text field focusable
-            replaceIn.setMaxLength(4096);
-            replaceIn.setText(be.getReplacementState());
-            root.add(replaceIn, 0, 20, 60, 1);
-
-            done = new WButton(new TranslatableText("gui.done"));
-            done.setOnClick(this::sendDataToServer);
-            root.add(done, 0, 31, 29, 1);
-            WButton cancel = new WButton(new TranslatableText("gui.cancel"));
-            cancel.setOnClick(() -> MinecraftClient.getInstance().openScreen(null));
-            root.add(cancel, 31, 31, 29, 1);
-
-            // disable saving when the loot table ID is not a valid Identifier
-            lootTableIn.setChangedListener(text -> done.setEnabled(Identifier.isValid(text)));
-        }
-
-        private void sendDataToServer() {
-            LootDataPacketData data =
-                new LootDataPacketData(backingBe.getPos(), lootTableIn.getText(), replaceIn.getText());
-            PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
-            data.write(buf);
-            ClientSidePacketRegistry.INSTANCE.sendToServer(ServerStructHelpPackets.LOOT_DATA_UPDATE, buf);
-            MinecraftClient.getInstance().openScreen(null);
-        }
+    private void sendDataToServer() {
+        LootDataPacketData data =
+            new LootDataPacketData(backingBe.getPos(), lootTableIn.getText(), replaceIn.getText());
+        PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
+        data.write(buf);
+        ClientSidePacketRegistry.INSTANCE.sendToServer(ServerStructHelpPackets.LOOT_DATA_UPDATE, buf);
+        this.onClose();
     }
 }
